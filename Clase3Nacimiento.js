bda4d5f0bcf7a9d624e46510c2eeac94a2153fae
ppablomn <90877741+ppablomn@users.noreply.github.com>
2021-09-28 00:13:18 -0300

    Add files via upload

---
+++ Clase3Nacimiento.js
@@ -0,0 +1,7 @@
+
+let palabra = prompt("Por favor ingrese la palabra correcta");
+while(palabra != "coderhouse"){
+    palabra = prompt("Por favor ingrese la palabra correcta");
+
+}
+alert("Has ingresado la palabra correcta");
